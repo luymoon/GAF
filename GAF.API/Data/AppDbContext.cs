32aca64d56e24c6f3f06516292e13369e20d1080
luymoon <aguiarluana780@gmail.com>
2025-08-22 11:37:42 -0300

    sejam respeitosos um com os outros, e amem os outros como a seus pais

--- GAF.API/Data/AppDbContext.cs
+++ GAF.API/Data/AppDbContext.cs
@@ -1,5 +1,7 @@
 using GAF.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace GAF.API.Data;
 
@@ -12,5 +14,63 @@ public class AppDbContext : IdentityDbContext<User>
 
     public DbSet<Transaction> Transactions { get; set; }
     public DbSet<Category> Categories { get; set; }
-    public DbSet<Report> Reports { get; set; }       
+    public DbSet<Report> Reports { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        SeedDefaultUser(builder);
+    }
+
+    private static void SeedDefaultUser(ModelBuilder builder)
+    {
+        #region Populate Roles - Perfis de Usuário
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole() {
+               Id = "0b44ca04-f6b0-4a8f-a953-1f2330d30894",
+               Name = "Administrador",
+               NormalizedName = "ADMINISTRADOR"
+            },
+            new IdentityRole() {
+               Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
+               Name = "Usuário",
+               NormalizedName = "USUÁRIO"
+            },
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+        #endregion
+
+        #region Populate Usuário
+        List<Usuario> usuarios = new() {
+            new Usuario(){
+                Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
+                Email = "gallojunior@gmail.com",
+                NormalizedEmail = "GALLOJUNIOR@GMAIL.COM",
+                UserName = "LuanaJunior",
+                NormalizedUserName = "LUANAJUNIOR",
+                LockoutEnabled = true,
+                EmailConfirmed = true,
+                Name = "Luana Da Silva Junior"
+            }
+        };
+        foreach (var user in usuarios)
+        {
+            PasswordHasher<IdentityUser> pass = new();
+            user.PasswordHash = pass.HashPassword(user, "123456");
+        }
+        builder.Entity<User>().HasData(usuarios);
+        #endregion
+
+        #region Populate UserRole - Usuário com Perfil
+        List<IdentityUserRole<string>> userRoles = new()
+        {
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[0].Id
+            },
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        #endregion
+    }
 }
